4d1f12767db84b93d8072681a57b2c2a2c7881c9
omoladeajala <omoladeajala2021@gmail.com>
2025-09-30 16:25:33 +0100

    Refactor App component and remove unused assets

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const fags = [
+  {
+    title: "Where are these chairs assemble?",
+    text: "Where they are meant to be.",
+  },
+  {
+    title: "How long do i have to return the chairs?",
+    text: "You have just an hour!",
+  },
+  {
+    title: "Do you ship to country outside EU?",
+    text: "Yes of course.",
+  }
+];
+export default function App() {
+return (
+  <div>
+    <Accordion />
+  </div>
+);
 }
 
-export default App;
+function Accordion() {
+  return <div>TODO</div>
+}
+
+function AccordionItem({num, title, text}) {
+  return <div className='item'>
+    <p className='number'>{num}</p>
+  </div>
+}
