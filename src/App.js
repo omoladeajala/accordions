615e71dc94e9e0e1054400a9dc60bb96d571b22d
omoladeajala <omoladeajala2021@gmail.com>
2025-10-16 00:28:07 +0100

    implement Accordion and AccordionItem components - Added reusable Accordion component to display FAQ data - Implemented toggle logic using useState for item expansion - Rendered dynamic titles and content for each accordion item

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+
 import './App.css';
 import { useState } from "react";
 
@@ -11,25 +11,50 @@ const fags = [
     title: "How long do i have to return the chairs?",
     text: "You have just an hour!",
   },
-  {
+{
     title: "Do you ship to country outside EU?",
     text: "Yes of course.",
-  }
+  }, 
+  {
+    title: "What are you learning at the moment?",
+    text: "JavaScript and React.",
+  }  
 ];
+
 export default function App() {
 return (
   <div>
-    <Accordion />
+    <Accordion data={fags} />
   </div>
 );
 }
 
-function Accordion() {
-  return <div>TODO</div>
+function Accordion({data}) {
+  return (
+    <div className="accordion">
+      {data.map((el, i) => (
+        <AccordionItem 
+          title={el.title} 
+          text={el.text} 
+          num={i} key={el.title}
+      />))}
+  </div>
+  );
 }
 
 function AccordionItem({num, title, text}) {
-  return <div className='item'>
-    <p className='number'>{num}</p>
+  const [isOpen, setIsOpen] = useState(false); 
+
+function handleToggle() {
+  setIsOpen((isOpen) => !isOpen)
+}
+  return (
+     <div className={`item ${isOpen ? "open" : " "}`} onClick={handleToggle}>
+    <p className="numbe">{num < 9 ? `0${num + 1}` : num + 1}</p>
+    <p className="title">{title}</p>
+    <p className="icon">{isOpen ? "-" : "+" } </p>
+
+    {isOpen && <div className="context-box">{text} </div>}
   </div>
+  );
 }
